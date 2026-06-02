13afe673ed6176efcef73eba9fa2d6cd776f4b07
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-02 22:53:28 +0530

    navbar created

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/assets/Wanderlast.png";
+
+const Navbar = () => {
+  const links = [
+    { label: "Home", path: "/" },
+    { label: "Destinations", path: "/" },
+    { label: "My Bookings", path: "/" },
+    { label: "Admin", path: "/" },
+  ];
+
+  return (
+    <nav className="w-11/12 mx-auto flex justify-between items-center py-4">
+      <ul className="flex gap-4 items-center">
+        {links.map((link, ind) => (
+          <Link key={ind} href={link.path}>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div>
+        <Image src={logo} alt="Logo" width={150} height={150} />
+      </div>
+      <ul className="flex gap-4 items-center">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link href={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link href={"/signup"}>Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
