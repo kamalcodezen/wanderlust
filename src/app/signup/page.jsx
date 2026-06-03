0e2bfb950e41d75091a8eb4084617d49143f6d81
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-03 23:13:14 +0530

    better auth install and all file create

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import SignUp from "@/components/auth/SignUp";
+
+const SignUpPage = () => {
+  return (
+    <div className="min-h-screen">
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
